f80e8ddc14269711ad1573caf5db6eb59d1fe6c4
lilfool1994 <99862002+lilfool1994@users.noreply.github.com>
2022-03-25 22:55:13 -0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,139 @@
+#include <iostream>
+#include <stack>
+#include <iomanip>
+#include <iterator>
+#include <cmath>
+#include <string>
+#include <list>
+#include <cstring>
+#include <sstream>
+#include <chrono>
+#include <vector>
+#include <fstream>
+#include "input.h"
+
+using namespace std;
+
+
+
+class MyBag {
+private:
+    int* array;
+    int size;
+    
+public:
+    
+    MyBag()
+    {
+        array = nullptr;
+        size = 0;
+    }
+    MyBag(int* a, int newSize)
+    {
+        size = newSize;
+        array = new int[size];
+
+        for (int i = 0; i < size; i++)
+            array[i] = a[i];
+    }
+    ~MyBag()
+    {
+        delete[] array;
+    }
+
+    void display()
+    {
+        cout << endl << "Displaying array: \n";
+        
+        for (int i = 0; i < size; i++)
+            cout << array[i] << '\n';
+        
+        
+    }
+
+
+    void insert()
+    {
+       /* int newInt = inputInteger("Enter new integer: ");
+        for (int i = 0; i < size; i++) {
+            newInt = array[i++];
+        }*/
+        
+            int list[] = { inputInteger("Enter new integer: ") };
+            MyBag test1(list, 1);
+        
+        
+    }
+
+    
+
+    void sortArray()
+    {
+        cout << endl << "The sorted array is: \n";
+
+        sort(array, array + size);
+    }
+
+    
+
+     void clear()
+     {
+         size = 0;
+         cout << endl << "The array has been cleared. \n";
+    }
+
+     
+
+};
+
+int nonTemplateMenuOption()
+
+{
+    cout << endl << "1> Non-template MyBag of integers";
+    cout << endl << "==========================================";
+    cout << endl << "1 > clear";
+    cout << endl << "2 > insert";
+    cout << endl << "3 > search";
+    cout << endl << "4 > remove";
+    cout << endl << "5 > sort";
+    cout << endl << "6 > display";
+
+    cout << endl << "==========================================";
+    cout << endl << "0. Exit";
+    cout << endl << "==========================================" << endl;
+    int options = inputInteger("Options: ", 0, 6);
+    cout << endl << endl << endl;
+    return options;
+}
+
+
+
+
+void main()
+{
+    
+   
+    MyBag h;
+    
+    
+   
+
+    do
+    {
+        switch (nonTemplateMenuOption())
+        {
+        case 0: return; break;
+               case 1: h.clear(); break;
+               case 2:  h.insert(); break;
+             //  case 3: search(); break;
+             //  case 4: remove(); break;
+               case 5: h.sortArray(); break;
+               case 6: h.display(); break;
+        default: cout << "\t\tERROR - Invalid option. Please re-enter."; break;
+        }
+        cout << "\n";
+        system("pause");
+
+    } while (true);
+
+}
